5d9356a8deac62b7a1947ae6b02665c022f033c7
Anton Vlasenko <anton.vlasenko94@gmail.com>
2023-01-23 17:47:23 +0000

    Add <Header /> component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     //HTML
-    <header>reddit Lite</header>
+    <Header />
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header>
+      <div id="h1-container">
+        <h1><span id="reddit">reddit</span><span id="lite">Lite</span></h1>
+      </div>
+    </header>
+  );
+}
