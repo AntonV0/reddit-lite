8653975adf7c8d8c407cc344c38b852170b84229
Anton Vlasenko <anton.vlasenko94@gmail.com>
2023-01-23 18:45:42 +0000

    Update <Header /> component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
-import "./Header.css";
+import './Header.css';
 
 export default function Header() {
   return (
     <header>
-      <div id="h1-container">
-        <h1><span id="reddit">reddit</span><span id="lite">Lite</span></h1>
+      <div id='h1-container'>
+        <h1><span id='reddit'>reddit</span><span id='lite'>Lite</span></h1>
       </div>
     </header>
   );
