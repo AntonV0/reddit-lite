dfb2b5db749ac2d636052a0357882b314a3769a8
Anton Vlasenko <anton.vlasenko94@gmail.com>
2023-01-27 18:38:24 +0000

    Add <Post /> component

---
+++ src/components/Post.js
@@ -0,0 +1,10 @@
+import './Post.css';
+
+export default function Post() {
+  return (
+    <div class='post'>
+      <h2>Post Title</h2>
+      <p>Post info Post info Post info Post info Post info</p>
+    </div>
+  )
+}
