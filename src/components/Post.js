e0d4389c9199b09b02020b10360e0a1a3375e9bf
Anton Vlasenko <anton.vlasenko94@gmail.com>
2023-02-05 19:25:11 +0000

    Fetch JSON data from Reddit API

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,10 +1,48 @@
 import './Post.css';
+import comments from '../utils/images/comments.png';
+import scoreArrow from '../utils/images/scoreArrow.png';
+import formatNumber from '../utils/constants/formatNumber';
 
-export default function Post() {
+export default function Post({
+  score,
+  subreddit,
+  username,
+  title,
+  // subtitle
+  // media
+  mediaUrl,
+  isVideo,
+  commentsNum
+}) {
   return (
-    <div class='post'>
-      <h2>Post Title</h2>
-      <p>Post info Post info Post info Post info Post info</p>
-    </div>
-  )
+    <article className='fullPost'>
+      <div className='postOrigin'>
+        <img src={scoreArrow} className='arrowIcon' />
+        <p className='postScore' title='Reddit score'>{formatNumber(score)}</p>
+        <button className='postSubredditButton' title='Visit subreddit'>
+          <p className='postSubreddit' title='Visit subreddit'>
+            r/<span className='subredditName'>{subreddit}</span>
+          </p>
+        </button>
+        <p className='postUsername'>Posted by&nbsp;
+          <button className='usernameButton' title='View profile'>u/<span className='username'>{username}</span></button>
+        </p>
+      </div>
+      <h3 className='postText' id='postTitle'>{title}</h3>
+      <p className='postText' id='postSubtitle'>{/*subtitle*/}</p>
+
+      {/* <button className='readMoreButton'>Read more</button> */}
+
+      
+      {/* <img src={mediaUrl} className='postImage' alt='' /> */}
+      <video src={mediaUrl + '/DASH_600_'} className='postVideo' controls muted alt='' />
+      <div className='commentsSection'>
+        <button className='commentsButton' title='View Comments'>
+          <img src={comments} className='commentsIcon' />
+          <span className='commentsText'>{formatNumber(commentsNum)} Comments</span>
+        </button>
+        <button className='redditButton'>View on Reddit</button>
+      </div>
+    </article>
+  );
 }
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,48 +1,63 @@
+import React, { useState, useEffect } from 'react';
 import './Posts.css';
-// import Post from './Post.js';
-import MockPost1 from '../MockPosts/MockPost1';
-import MockPost2 from '../MockPosts/MockPost2';
-import MockPost3 from '../MockPosts/MockPost3';
-import MockPost4 from '../MockPosts/MockPost4';
-import MockPost5 from '../MockPosts/MockPost5';
-import MockPost6 from '../MockPosts/MockPost6';
+import Post from './Post.js';
 
 export default function Posts() {
+
+  const [posts, setPosts] = useState([]);
+  const [subreddit, setSubreddit] = useState('all');
+
+  useEffect(() => {
+    // Fetches Reddit JSON data of top posts from all subreddits
+    fetch('https://www.reddit.com/r/all/.json').then(response => {
+      // Checks if request has succeeded (HTTP 200 OK status)
+      if(response.status != 200) {
+        console.log('Error: Response status is not 200.');
+        return;
+      }
+      
+      response.json().then(data => {
+        if (data !== null) {
+          console.log(data); // Displays all post data when inspecting the browser console (25 posts)
+          setPosts(data.data.children); // Sets 'Posts' data equal to the 'children' array
+        }
+      })
+    })
+    // Currently the post data updates once on load
+  }, []/*, [subreddit]*/);
+
   return (
     <div>
-      <div class='postsContainer'>
-        <h2>Today's Top Reddit Posts</h2>
-        <div class='posts'>
-          <MockPost1 />
-          <MockPost2 />
-          <MockPost3 />
-          <MockPost4 />
-          <MockPost5 />
-          <MockPost6 />
-          <MockPost1 />
-          <MockPost2 />
-          <MockPost3 />
-          <MockPost4 />
-          <MockPost5 />
-          <MockPost1 />
-          <MockPost2 />
-          <MockPost3 />
-          <MockPost6 />
-          <MockPost5 />
-          <MockPost1 />
-          <MockPost2 />
-          <MockPost3 />
-          <MockPost4 />
-          <MockPost5 />
-          <MockPost6 />
-          <MockPost2 />
-          <MockPost3 />
-          <MockPost4 />
-          <MockPost5 />
+      <div className='postsContainer'>
+        <h2>Top Reddit Posts&nbsp;
+          <span className='postTime'>Today</span>&nbsp;/&nbsp;
+          <span className='postTime'>This Week</span>&nbsp;/&nbsp;
+          <span className='postTime'>This Month</span>&nbsp;/&nbsp;
+          <span className='postTime'>This Year</span>&nbsp;/&nbsp;
+          <span className='postTime'>All Time</span>
+        </h2>
+        <div className='posts'>
+
+          {(posts !== null) ? posts.map((post) => 
+            <Post 
+              key={post.data.id} 
+              score={post.data.score}
+              subreddit={post.data.subreddit}
+              username={post.data.author}
+              title={post.data.title}
+              // subtitle={post.data.} ???
+              // media={post.data.media}
+              mediaUrl={post.data.url}
+              isVideo={post.data.is_video}
+              commentsNum={post.data.num_comments}
+            />
+          ) : ''}
+
+         
         </div>
       </div>
-      <div class='loadMoreButtonContainer'>
-        <button class='loadMoreButton'>Load More Posts</button>
+      <div className='loadMoreButtonContainer'>
+        <button className='loadMoreButton'>Load More Posts</button>
       </div>
     </div>
   );
